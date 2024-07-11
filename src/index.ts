4683694dae7c7ffdbfdfb846898474247c368f1f
Vyapak Khare <vyapakkhare@Vyapaks-MacBook-Pro.local>
2024-07-12 03:39:53 +0530

    [fix] issue

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
+    const {} = req.body;
     res.send('Hello, TypeScript Express!');
 });
 
 app.get('/health', (req: Request, res: Response) => {
+    const {} = req.body;
     res.status(200).send('Healthy');
 });
 
